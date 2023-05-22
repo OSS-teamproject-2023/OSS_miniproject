3e1daac6bd9012643b8bd5d7b495124f82432c9b
Kim Eun Taek <21700147@handong.ac.kr>
2023-05-22 17:04:51 +0900

    make transaction.c

--- transaction.h
+++ transaction.h
@@ -18,3 +18,10 @@ int loadTransaction(Transaction *t[], int *count);
 void searchTransaction(Transaction *t[], int count);
 double krw_to_usd(double krw);
 void exchageTransaction(Transaction *t[], int count);
+void monthlySpend(Transaction *t[], int count);
+void searchByCategory(Transaction *t[], int count);
+void swap(Transaction **a, Transaction **b);
+void amountSorting(Transaction *t [], int count);
+void mostSpendCategory(Transaction *t [], int count);
+void yearSummary(Transaction *t [], int count);
+
